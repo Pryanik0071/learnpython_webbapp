c2d71cfc84c3950fc449420a54307d962d733863
Dmitry <di.mesler.2010@yandex.ru>
2021-10-04 12:47:43 +0300

    Add: Show|hide listLinks

--- webapp/static/js/main.js
+++ webapp/static/js/main.js
@@ -8,7 +8,31 @@ $(document).ready(function () {
     // document.querySelector("body").classList.toggle("body-overflow-hidden");
   });
 
+  // Тень для header при скролле
   $(window).scroll(function () {
     $("header").toggleClass("scroll", $(this).scrollTop() > 100);
+  });
+
+  // Скрывать/показывать список ссылок при клике на заголовок
+  $(".nav__link").on("click", function () {
+    var listLinks = $(this).data("category");
+    $(".nav__link__list").hide();
+    $(`[data-category='${listLinks}']`).show();
+  });
+
+  // Закрыть список ссылок при клике на Esc
+  document.addEventListener("keyup", closeModal);
+  function closeModal(event) {
+    if (event.key === "Escape" || event.type === "click") {
+      $(".nav__link__list").hide();
+    }
+  }
+
+  // Закрыть список ссылок при клике вне области ссылок
+  $(document).mouseup(function (e) {
+    var container = $(".nav__link__list");
+    if (container.has(e.target).length === 0) {
+      container.hide();
+    }
   });
 });
