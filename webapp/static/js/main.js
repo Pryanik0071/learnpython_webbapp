3276835224248cbe3bf3a39152c66d218b37dbbb
Dmitry Mesler <41776673+Pryanik0071@users.noreply.github.com>
2021-10-22 03:35:52 +0300

    Feature/header (#2)
    * Add: main colors, fonts
    
    * Add: color for body
    
    * Add: header HTML + CSS
    
    * Add: adaptive header for 1200, 992 width
    
    * Add: adaptive header for 320-768 width
    
    * Add: box-shadow for header when scrolling page, fixed header + burger menu
    
    * Add: Show|hide listLinks
    
    * Add: styles for listLinks
    
    * Add: styles for mobile nav menu
    
    * Add: header animation click

---
+++ webapp/static/js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  // Кнопка в шапке
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", function () {
+    document
+      .querySelector(".nav--mobile--hidden")
+      .classList.toggle("nav--mobile--visible");
+    // document.querySelector("body").classList.toggle("body-overflow-hidden");
+  });
+
+  // Тень для header при скролле
+  $(window).scroll(function () {
+    $("header").toggleClass("scroll", $(this).scrollTop() > 100);
+  });
+
+  // Скрывать/показывать список ссылок при клике на заголовок
+  $(".nav__link").on("click", function () {
+    var listLinks = $(this).data("category");
+    $(".dropdown-menu").hide();
+    $(`[data-category='${listLinks}']`).show();
+  });
+
+  // Закрыть список ссылок при клике на Esc
+  document.addEventListener("keyup", closeModal);
+  function closeModal(event) {
+    if (event.key === "Escape" || event.type === "click") {
+      $(".dropdown-menu").hide();
+    }
+  }
+
+  // Закрыть список ссылок при клике вне области ссылок
+  $(document).mouseup(function (e) {
+    var container = $(".dropdown-menu");
+    if (container.has(e.target).length === 0) {
+      container.hide();
+    }
+  });
+});
