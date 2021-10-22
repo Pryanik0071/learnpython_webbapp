f7a9542380dd4f5ce3ef06b6d96bd61ee004b404
Dmitry <di.mesler.2010@yandex.ru>
2021-10-22 03:01:17 +0300

    BugFix: models: Book, Category

--- webapp/model.py
+++ webapp/model.py
@@ -4,6 +4,12 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 
+book_category = db.Table('book_category',
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
+
+
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -14,12 +20,10 @@ class Book(db.Model):
     #                          nullable=False)
     price = db.Column(db.Numeric(10, 2))
     description = db.Column(db.String)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'),
-                            nullable=False)
+    category_id = db.relationship('Category', secondary=book_category, lazy='subquery',
+                                  backref=db.backref('books', lazy=True))
     url = db.Column(db.String, unique=True, nullable=False)
     text = db.Column(db.Text, nullable=True)
-
-    categories = db.relationship("BookCategory")
 
     def __repr__(self):
         return f'Book: {self.title} - id: {self.id}'
@@ -29,17 +33,5 @@ class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
-    books = db.relationship("BookCategory")
-
     def __repr__(self):
         return f'Category: {self.name} - id: {self.id}'
-
-
-class BookCategory(db.Model):
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), index=True, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
-    book = db.relationship("Book", lazy='joined')
-    category = db.relationship("Category", lazy='joined')
-
-    def __repr__(self):
-        return f"Book.id: {self.book_id} Category.id: {self.category_id}"
