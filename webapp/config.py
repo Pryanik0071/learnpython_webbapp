1da76a99d7a0ca594d5d32e116e46bc513c84812
Dmitry <di.mesler.2010@yandex.ru>
2021-10-27 01:37:48 +0300

    Delete: Config file (test)

--- webapp/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-config_dir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = ('sqlite:///' +
-                           os.path.join(config_dir, '..', 'webapp.db'))
