89855949c06af28fab4334ad1a3b77b870e4013a
Dmitry <di.mesler.2010@yandex.ru>
2021-10-27 01:38:28 +0300

    Add: Book insert route

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 from webapp.model import db
+from webapp.insert_books import insert_books_db
 
 
 def create_app():
@@ -11,5 +12,14 @@ def create_app():
     @app.route('/')
     def index():
         return render_template('base.html')
+
+    @app.route('/get-books/', methods=['POST'])
+    def get_books():
+        data = request.get_json()
+        authorization_header = request.headers.get('Authorization')
+        if authorization_header == app.config['AUTHORIZATION_TOKEN']:
+            insert_books_db(data)
+            return jsonify(data)
+        return jsonify({'detail': 'authentication failed'})
 
     return app
