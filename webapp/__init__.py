f52e0cee020dca4cfd02d153c5d0f71ac71b30fe
Dmitry <di.mesler.2010@yandex.ru>
2021-11-11 12:53:23 +0300

    Refactoring: Filters, add filter by category

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -27,48 +27,27 @@ def create_app():
         years_list = db.session.query(Book.year).group_by(Book.year).order_by(Book.year.desc())
         publisher_list = Publisher.query.all()
         query = db.session.query(Book).order_by(Book.year.desc())
-        year = request.args.get('year')
-        publisher = request.args.get('publisher')
-        language = request.args.get('language')
-        category = request.args.get('category')
         skill = request.args.get('skill')
+        language = request.args.get('language')
+        publisher = request.args.get('publisher')
+        theme = request.args.get('theme')
+        year = request.args.get('year')
 
-        if year:
-            query = query.filter(Book.year == year)
-        if publisher:
-            query = query.join(Publisher).filter(Publisher.title == publisher)
-        if language:
-            query = query.filter(Book.categories.any(name=language))
-        if category:
-            query = query.filter(Book.categories.any(name=category))
         if skill:
             query = query.filter(Book.categories.any(name=skill))
+        if language:
+            query = query.filter(Book.categories.any(name=language))
+        if theme:
+            query = query.filter(Book.categories.any(name=theme))
+        if publisher:
+            query = query.join(Publisher).filter(Publisher.title == publisher)
+        if year:
+            query = query.filter(Book.year == year)
 
         book_list = query.all()
 
         return render_template('books.html', book_list=book_list,
                                years_list=years_list, publishers=publisher_list)
-
-    # @app.route('/books/')
-    # def books():
-    #     query = db.session.query(Book)
-    #     year = request.args.get('year')
-    #     min_price = request.args.get('min_price')
-    #
-    #     if year:
-    #         query = query.filter(Book.year == year)
-    #
-    #     if min_price:
-    #         min_price = int(min_price)
-    #         query = query.filter(Book.price >= min_price)
-    #
-    #     book_list = query.all()
-    #
-    #     # book_list = db.session.query(Book).join(Publisher).all()
-    #     # authors = book_list.book_author
-    #     # print(authors)
-    #     # print(book_list)
-    #     return render_template('books.html', book_list=book_list)
 
     @app.route('/book/<int:id_book>/')
     def book_info(id_book):
